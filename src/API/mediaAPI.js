0bb32d1acd1e387df6b456b2f0e22089513daf30
Soham Mondal <sohammondal821@gmail.com>
2026-01-18 17:20:24 +0530

    searchSlice-Done

--- src/API/mediaAPI.js
+++ src/API/mediaAPI.js
@@ -49,7 +49,7 @@ export async function fetchVideos(query, page = 1, per_page = 15) {
                 per_page
             },
             headers: {
-                Authorization: `Client-Id ${PEXELS_KEY}`
+                Authorization: `Client-ID ${PEXELS_KEY}`
             }
         })
 
